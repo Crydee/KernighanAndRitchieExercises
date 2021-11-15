b00db5995edb7a8d2ca5699bbca4c16477eabea8
oms <oscar.sterland@metaswitch.com>
2021-11-15 21:57:24 +0000

    Response to some markups.

--- inputOutput/ex1-12/ex1-12.c
+++ inputOutput/ex1-12/ex1-12.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
 /* A program that prints its input one word per line. */
 
 main() {
 
-int state, c;
-state = OUT;
+int c;
 
 while ((c = getchar()) != EOF) {
-    if ( c != ' ' && c != '\n' && c != '\t') {
+    if ((c != ' ') && (c != '\n') && (c != '\t')) {
         putchar(c);
-        state = IN;
-    } else if (state == IN) {
+    } else {
         putchar('\n');
-        state = OUT;
+        while (((c = getchar()) != EOF) && ((c == ' ') || (c == '\n') || (c == '\t')))
+        ;
+        putchar(c);
     }
 }
 putchar('\n');
