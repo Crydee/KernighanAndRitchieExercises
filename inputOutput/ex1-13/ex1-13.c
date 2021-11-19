e700499c5fd3615b0e73d0d72d2374c2cbe88b8c
oms <oscar.sterland@metaswitch.com>
2021-11-19 20:28:20 +0000

    Debugged error where all words were counted as one letter long

--- inputOutput/ex1-13/ex1-13.c
+++ inputOutput/ex1-13/ex1-13.c
@@ -1,17 +1,14 @@
 /* A program to print a histogram of the lengths of words in its input. */
 #include <stdio.h>
 
-#define INWORD 1
-#define PREVCHARWSPACE 0
 #define MAXWORDLEN 10
 
 main() {
 
 int lenfreqs[MAXWORDLEN + 1]; /* This array will store the the frequencies with which words of lengths 1, ..., MAXWORDLEN, MAXWORDLEN+ appear */
-int c, hit_EOF, l, /*state*/ wordlen;
+int c, hit_EOF, l, wordlen;
 
 hit_EOF = 0;
-/*state = OUT;*/
 wordlen = 0;
 
 for( l = 0; l <= MAXWORDLEN; ++l) {
@@ -19,24 +16,23 @@ for( l = 0; l <= MAXWORDLEN; ++l) {
 }
 
 while (hit_EOF == 0) {
-    c = getchar();
-    if ( c == ' ' || c == '\n' || c == '\t') {
-        if( wordlen != 0){
-            ++lenfreqs[wordlen - 1];
-            wordlen = 0;
-            /*state = OUT;*/}
+  c = getchar();
+  if ((c == ' ') || (c == '\n') || (c == '\t') || (c == EOF)) {
+    if((wordlen != 0) && (wordlen <= MAXWORDLEN)){
+          ++lenfreqs[wordlen - 1];
+          wordlen = 0;
+          }
+    else if((wordlen != 0) && (wordlen >= MAXWORDLEN)) {
+        ++lenfreqs[MAXWORDLEN];
     }
-    else if (c == EOF) {
-        if( wordlen != 0){
-            ++lenfreqs[wordlen - 1];
-            wordlen = 0;
-            /*state = OUT;*/}
-        hit_EOF = 1;
-    }
-    else {
-        ++wordlen;
-    }
+    if (c == EOF) {
+          hit_EOF = 1;
+      }
+  }
+  else
+      ++wordlen;
 }
 for( l = 0; l <= MAXWORDLEN; ++l)
     printf(" %d", lenfreqs[l]);
+    putchar('\n');
 }
