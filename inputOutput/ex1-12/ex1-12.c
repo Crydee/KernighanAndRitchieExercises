30082b1896e5d27d608c95a323857f350a4c5bb6
Crydee <oscarsterland@yahoo.co.uk>
2022-06-05 19:44:13 +0100

    some misc improvements

--- inputOutput/ex1-12/ex1-12.c
+++ inputOutput/ex1-12/ex1-12.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/* A program that prints its input one word per line. */
+/* A program that prints its input one 'word' per line, where a 'word' is a continuous sequence of */
+/* non-whitespace characters */
 
 main() {
 
@@ -8,12 +9,16 @@ int c;
 
 while ((c = getchar()) != EOF) {
     if ((c != ' ') && (c != '\n') && (c != '\t')) {
+        /* We always want to just print non-whitespace characters. */
         putchar(c);
     } else {
         putchar('\n');
         while (((c = getchar()) != EOF) && ((c == ' ') || (c == '\n') || (c == '\t')))
+        /* Here the last char we printed was a new-line.  We don't want to print more newlines until we have */
+        /* printed another 'word'. */
         ;
-        putchar(c);
+        if (c != EOF)
+            putchar(c);
     }
 }
 putchar('\n');
--- inputOutput/ex1-14/ex1-14.c
+++ inputOutput/ex1-14/ex1-14.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
+/* print a histogram of the frequencies of different characters in input. */
 
+/* We assume that the values of all of the visible characters are a consecutive sequence of increasing */
+/* positive integers between the value for the character of ! and the value for the character of ~ */
 main() {
 
-int c, col, i, max_freq, row;
-int char_freq_arr[78];
+int c, col, i, max_freq, num_vis_chars, row;
+num_vis_chars = ('~' - '!') + 1;
+printf("the val of num_vis_chars: %d\n", num_vis_chars);
+int char_freq_arr[num_vis_chars];
 
-for (i=0; i < 78; i++) {
+for (i=0; i < num_vis_chars; i++) {
   char_freq_arr[i] = 0;
 }
 max_freq = 0;
 
 while ((c = getchar()) != EOF) {
-  if ((48 <= c) && (c <= 126)) {
-    ++char_freq_arr[c - 48];
-    if (char_freq_arr[c - 48] > max_freq)
-      max_freq = char_freq_arr[c - 48];
+  if (('!' <= c) && (c <= '~')) {
+    ++char_freq_arr[c - '!'];
+    if (char_freq_arr[c - '!'] > max_freq)
+      max_freq = char_freq_arr[c - '!'];
     }
   }
 
-/*for (i = 0; i < 78; i++) {
-  printf("%d", char_freq_arr[i]);  
-}*/
-
-
-
-
-/*printf("The value of 'c' is %d and the value of d is %d\n", 'c', 'd');*/
-
-for( row=0; row<max_freq; ++row) {
+for (row = 0; row < max_freq; ++row) {
   printf("%3d", max_freq - row);
-  for( col=0; col<78; ++col) {
-    if( char_freq_arr[col] >= (max_freq - row))
+  for (col = 0; col < num_vis_chars; ++col) {
+    if (char_freq_arr[col] >= (max_freq - row))
       printf("*");      
     else
       printf(" ");
   }
   printf("\n");
 }
-printf("  ");
-for (i = 48; i < 127; i++) {
+printf("   "); /* Three spaces so that the horizontal axis aligns with the histogram */
+for (i = '!'; i <= '~'; i++) {
   printf("%c", i);
   }
 printf("\n");
