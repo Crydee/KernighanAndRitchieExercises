c8149fc2e84fd053690ecd52b59f5d80923f1248
oms <oscar.sterland@metaswitch.com>
2022-06-08 19:50:59 +0100

    clarifications

--- tempTable/ex1-15/ex1-15.c
+++ tempTable/ex1-15/ex1-15.c
@@ -2,22 +2,23 @@
 
 /* fahr_to_cels: convert a temperature in Fahrenheit to a temperature in Celsius */
 
+#define LOWER 0 /* lower limit of temperature table */
+#define UPPER 300 /* upper limit of temperature table */
+#define STEP 20 /* step size */
+
 float fahr_to_cels(float fahr);
 
 main() {
 float fahr;
 
-#define LOWER 0 /* lower limit of temperature table */
-#define UPPER 300 /* upper limit of temperature table */
-#define STEP 20 /* step size */
-
 printf("Fahrenheit\t Celsius\n");
 
-for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) 
   printf("%7.0f\t%10.1f\n", fahr, fahr_to_cels(fahr) );
-}
+
+return 0;
 }
 
 float fahr_to_cels(float fahr) {
-  return 5 * (fahr - 32) / 9;
+  return (5.0/9.0) * (fahr - 32.0);
 }
