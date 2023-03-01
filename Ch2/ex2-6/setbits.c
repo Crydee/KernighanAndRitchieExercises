f9d894d75648c37b4970f0af81a52319c37d1135
Crydee <oscarsterland@yahoo.co.uk>
2023-03-01 19:37:47 +0000

    set up dir

---
+++ Ch2/ex2-6/setbits.c
@@ -0,0 +1,12 @@
+/* Write a function setbits(x, p, n, y) that returns x with the n bits that began at position p set to the rightmost n bits of y, leaving the */
+/* other bits unchanged. */
+
+#include <stdio.h>
+
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+
+main() {}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y) {
+
+}
