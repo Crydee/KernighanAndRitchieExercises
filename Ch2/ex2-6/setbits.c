da89bb2b70ad4db261de10266ad5a22f39632d4c
Crydee <oscarsterland@yahoo.co.uk>
2023-03-04 00:20:13 +0000

    initial go

--- Ch2/ex2-6/setbits.c
+++ Ch2/ex2-6/setbits.c
@@ -2,15 +2,47 @@
 /* other bits unchanged.  We assume that the rightmost bit of x is at position 0. */
 
 #include <stdio.h>
+#include <limits.h>
 
+void print_bits(unsigned x);
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 main() {
- int n = 4, p = 4;
- unsigned mask = (~(~0 << n)) << p + 1 - n;
-  printf("mask is %d\n", mask);
+  unsigned x, y, clearing_mask, setting_mask;
+  int n, p;
+
+  n = 10;
+  p = 12;
+
+  x = 0;
+  y = ~0;
+  printf("p is %d, n is %d\n", p, n);
+  printf("The binary representation of x = %d is: ", x);
+  print_bits(x);
+
+  printf("The binary representation of y = %d is: ", y);
+  print_bits(y);
+
+  x = setbits(x, p, n, y); 
+  print_bits(x);
 }
 
+void print_bits(unsigned x) {
+  int shift = 0, lim = sizeof(unsigned) * 8;
+  
+  while (shift < (lim))
+    printf("%d", (x >> ((lim - 1) - shift++)) & 1);
+  printf("\n");
+}
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
-  unsigned mask = (~(~0 << n)) << p + 1 - n;
+  unsigned clearing_mask = ~(~(~0 << n) << (p + 1 - n));
+  unsigned setting_mask = (~(~0 << n) & y) << (p + 1 - n);
+
+  printf("The clearing mask is: ");
+  print_bits(clearing_mask);
+  printf("The setting mask is: ");
+  print_bits(setting_mask);
+  printf("After the tansforation, x's binary representation is: ");
+
+  return ((x & clearing_mask) | setting_mask);
 }
