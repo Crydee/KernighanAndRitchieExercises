418d84c52bb91532ceb8038d13faba7daa1645d9
Crydee <oscarsterland@yahoo.co.uk>
2023-03-08 19:16:36 +0000

    First attempt, broken

--- Ch2/ex2-8/rotate.c
+++ Ch2/ex2-8/rotate.c
@@ -1,7 +1,19 @@
-
+#include <stdio.h>
 unsigned rotate(unsigned x, int n);
+void print_bits(unsigned x);
 
-main() {}
+main() {
+  unsigned n = 0177, k = 1;
+
+  printf("n is %o ", n);
+  printf("with bit pattern: \n");
+  print_bits(n);
+
+  printf("Rotating by %d bits gives: ", k);
+  n = rotate(n, k);
+  print_bits(n);
+}
+
 unsigned rotate(unsigned x, int n) {
   int i;
 
@@ -12,5 +24,13 @@ unsigned rotate(unsigned x, int n) {
       x = x >> 1;
   }
   return x;
+}
+
+void print_bits(unsigned x) {
+  int shift = 0, lim = sizeof(unsigned) * 8;
+  
+  while (shift < (lim))
+    printf("%d", (x >> ((lim - 1) - shift++)) & 1);
+  printf("\n");
 }
 
