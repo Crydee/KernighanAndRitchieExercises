4a2f4651a88338f7f1e4fd82bf14220983053b35
Crydee <oscarsterland@yahoo.co.uk>
2023-06-20 15:55:48 +0100

    sort it out

--- Ch3/ex3-3/expand.c
+++ Ch3/ex3-3/expand.c
@@ -5,9 +5,9 @@
 #include <stdio.h>
 
 int expand(char s1[], char s2[]);
-int is_int(char c);
-int is_lalph(char c);
-int is_ualph(char c);
+int are_ints(char c, char d);
+int are_lalph(char c, char d);
+int are_ualph(char c, char d);
 
 int main() {
   char test_in[1000] = "a-b and c-f and 1-92-4 some- -foo", test_out[1000];
@@ -21,49 +21,50 @@ int main() {
 int expand(char in[],char expanded[]) {
   int i, j, k;
 
-  for (i = j = 0; in[i] != '\0'; i++, j++) {
-    if (i > 0 && in[i] == '-') {
-      /* this may be a shorthand that we want to expand, so check the values on either side. */
-      if ((in[i-1] < in[i+1]) && ((is_int(in[i-1]) && is_int(in[i+1])) || (is_lalph(in[i-1]) && is_lalph(in[i+1])) || (is_ualph(in[i-1]) && is_ualph(in[i+1])))) {
-        /* we have a - separating values of 'matching type' so write the expansion */
-        printf("expanding\n");
-        for (k = in[i-1] + 1; k <= in[i+1];j++, k++) {
-          printf("writing  %c  to the arr\n", k);
-          expanded[j] = k;
-        }
-        j++;
+  for (i = j = 0; in[i] != '\0'; i++) {
+
+    if ((i != 0) && (in[i] == '-')) {
+      /* check if this is a short-hand that we want to expand. */
+      if ((in[i-1] < in[i+1]) && (are_ints(in[i-1], in[i+1]) || are_lalph(in[i-1], in[i+1]) || are_ualph(in[i-1], in[i+1]))) {
+        /* we want to expand this shorthand.  Since we've already written the member of the expansion with the smallest valued character constant, we don't want to print it again.
+         * We also don't want to write the upper most value as we'll write that when we go round the outer for loop again.  We just want to write the values between the two
+         * bounds, if any exist. */
+        k = 1;
+        while (k < (in[i+1] - in[i-1]))
+          expanded[j++] = (in[i-1] + k++);
       } else {
-        expanded[j] = in[i];
+        expanded[j++] = in[i];
       }
     } else {
-        expanded[j] = in[i];
+      expanded[j++] = in[i];
     }
   }
+  expanded[j++] = in[i];
   return j;
 }
 
-int is_int(char c) {
+int are_ints(char c, char d) {
   int ret_val = 0;
 
-  if (('0' <= c) && ('9' >= c))
+  if ((('0' <= c) && ('9' >= c)) && (('0' <= d) && ('9' >= d)))
     ret_val = 1;
 
   return ret_val;
 }
 
-int is_lalph(char c) {
+int are_lalph(char c, char d) {
   int ret_val = 0;
 
-  if (('a' <= c) && ('z' >= c))
+  if ((('a' <= c) && ('z' >= c)) && (('a' <= d) && ('z' >= d)))
     ret_val = 1;
 
   return ret_val;
 }
 
-int is_ualph(char c) {
+int are_ualph(char c, char d) {
   int ret_val = 0;
 
-  if (('A' <= c) && ('Z' >= c))
+  if ((('A' <= c) && ('Z' >= c)) && (('A' <= d) && ('Z' >= d)))
     ret_val = 1;
 
   return ret_val;
