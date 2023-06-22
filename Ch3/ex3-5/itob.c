57798edd9820033d39c69e0672c625dd62d56a24
Crydee <oscarsterland@yahoo.co.uk>
2023-06-23 00:14:44 +0100

    Handle negative numbers and refactor

--- Ch3/ex3-5/itob.c
+++ Ch3/ex3-5/itob.c
@@ -6,7 +6,7 @@ int itob(int n, char s[], int b);
 int reverse(char s[]);
 
 int main(){
-  int input_int = 27, radix = 9;
+  int input_int = -20, radix = 9;
   char output_str[20];
 
   itob(input_int, output_str, radix);
@@ -14,12 +14,19 @@ int main(){
 }
 
 int itob(int n, char s[], int b) {
-  int i = 0, quotient;
+  int i = 0, sign;
 
-  while (n > 0) {
-    s[i++] = ('0' + (n % b));
+  if ((sign = n) > 0)
+    n = -n;
+
+  while (n < 0) {
+    s[i++] = ('0' - (n % b));
     n /= b;
   }
+
+  if (sign < 0)
+    s[i++] = '-';
+
   reverse(s);
   return i;
 }
