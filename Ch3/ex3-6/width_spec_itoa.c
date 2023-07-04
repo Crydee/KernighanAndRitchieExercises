f74e8a1d9ee16bef205ce1d04538cbc949bc3dc5
Crydee <oscarsterland@yahoo.co.uk>
2023-07-04 13:15:46 +0100

    Finish exercise

--- Ch3/ex3-6/width_spec_itoa.c
+++ Ch3/ex3-6/width_spec_itoa.c
@@ -1,14 +1,21 @@
-/*Write a version of itoa that accepts three arguments instead of two.  The third argument is a minimum field witdh; the converted number must be padded with blanks on the left if necessary
+/*Write a version of itoa that accepts three arguments instead of two.  The third argument is a minimum field width; the converted number must be padded with blanks on the left if necessary
  * to make it wide enough.*/
 
 #include <stdio.h>
 
-void itoa(int n, char [s], int width);
+void itoa(int n, char s[], int width);
 int reverse(char s[]);
 
-int main() {}
+int main() {
+  char num_str[1000];
+  int num= -12345, width=17;
 
-void itoa(int n, char [s], int width) {
+  itoa(num, num_str, width);
+  printf("The int is %d, width is %d, the string representation is: |%s\n", num, width, num_str);
+
+}
+
+void itoa(int n, char s[], int width) {
   int i, sign;
 
   if ((sign = n) < 0) /* record sign */
@@ -19,6 +26,8 @@ void itoa(int n, char [s], int width) {
   } while ((n /= 10) > 0);
   if (sign < 0)
     s[i++] = '-';
+  while ((width - i) > 0)  /* pad the output to the desired width, if it is too short */
+    s[i++] = ' ';
   s[i] = '\0';
   reverse(s);
 }
