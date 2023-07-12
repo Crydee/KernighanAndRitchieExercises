44c54dd7b281cfa71d9843a920192dfcf1f42713
Crydee <oscarsterland@yahoo.co.uk>
2023-07-12 16:35:22 +0100

    add some bits

--- Ch3/ex3-4/largest_negative_itoa.c
+++ Ch3/ex3-4/largest_negative_itoa.c
@@ -4,3 +4,24 @@
 /*  The problem is that we make n positive if it's negative, but a two's complement representation can represent, for a given wordsize, a negative
  *  value with magnitude one larger than the largest positive value. The value of the largest +ve value that can be represented is
  *  2^(wordsize - 1) - 1*/
+
+void itoa(int n, char s[]);
+void mod_itoa(int n, char s[]);
+
+#include <stdio.h>
+
+void mod_itoa(int n, char s[]) {}
+
+void itoa(int n, char s[], int width) {
+  int i, sign;
+
+  if ((sign = n) < 0) /* record sign */
+    n = -n;           /* make n +ve */
+  i = 0;
+  do {                /* generate digits in reverse order */
+    s[i++] = n % 10 + '0';  /* get next digit */
+  } while ((n /= 10) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  reverse(s);
+}
