2a2b0125467021f00cc3d4a094f0b7e029e8ed53
Crydee <oscarsterland@yahoo.co.uk>
2023-11-14 21:03:40 +0000

    Add the modulus operator

--- Ch4/ex4-3/modulus_op.c
+++ Ch4/ex4-3/modulus_op.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h> /* for atof() - in K&R, math.h is referenced - this is an anachronism */
+#include <math.h>
 
 #define MAXOP 100 /* max size of operand or operator */
 #define NUMBER '0' /* signal that a number was found */
@@ -41,6 +42,13 @@ int main(void)
         op2 = pop();
         if(op2 != 0.0)
           push(pop() / op2);
+        else
+          printf("error: zero divisor\n");
+        break;
+      case '%':
+        op2 = pop();
+        if(op2 != 0.0)
+          push(fmod(pop(), op2));
         else
           printf("error: zero divisor\n");
         break;
