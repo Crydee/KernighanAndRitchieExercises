65aef0d517697eb47cb1d7c2a261219710805dc6
Crydee <93712280+Crydee@users.noreply.github.com>
2023-12-17 01:36:33 +0000

    Implement recursive soln (#46)

--- Ch4/ex4-13/recursive_reverse.c
+++ Ch4/ex4-13/recursive_reverse.c
@@ -1,26 +1,34 @@
 /* Write a recursive  ersion of teh function reverse(s), which reerrses the string s in place. */
 
 #include <stdio.h>
+#include <string.h>
 
-void reverse(char s[], int lower, int upper);
+void reverse(char s[]);
+void rec_reverse(char *, int i, int j);
 
-int main() {
-  char input[] = "abcdefg";
-
-  printf("The result of calling reverse on %s ", input);
-  reverse(input, 0, 6);
-  printf("is %s\n", input);
-
+int main(int argc, char *argv[])
+{
+  printf("The un-reversed string is %s ", argv[1]);
+  reverse(argv[1]);
+  printf("The reversed string is: %s\n", argv[1]);
 }
 
-void reverse(char s[], int lower, int upper) {
-  int swap;
 
-  if (lower < upper) {
-    reverse(s, lower + 1, upper - 1);
-    swap = s[lower];
-    s[lower] = s[upper];
-    s[upper] = swap;
-  }
+void rec_reverse(char *s, int i, int j)
+{
+  char temp;
+
+  if (i >= j)
+    return;
+
+  temp = s[j];
+  s[j] = s[i];
+  s[i] = temp;
+  rec_reverse(s, ++i, --j);
 }
 
+void reverse(char s[])
+{
+  int i = 0, j = strlen(s);
+  rec_reverse(s, i, --j);
+}
