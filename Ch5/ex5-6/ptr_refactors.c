f20a935388c889e3edd920b83e3e07ec1b516d40
Crydee <oscarsterland@yahoo.co.uk>
2023-12-23 02:59:37 +0000

    Implement ptr version of atoi

--- Ch5/ex5-6/ptr_refactors.c
+++ Ch5/ex5-6/ptr_refactors.c
@@ -1,15 +1,18 @@
 /* rewrite appropriate programs from earlier chapters and exercises with pointers instead of array indexing.  Good possibilities
  * include getline, atoi, itoa, and their variants, reverse, and strindex and getop. */
 
+#include <ctype.h>
 #include <stdio.h>
 
+int ptr_atoi(char *s);
 int ptr_getline(char *s, int lim);
 
 int main() {
-  char input[100];
+  char input[100], atoi_in[] = "12345";
 
   ptr_getline(input, 10);
   printf("Input was %s", input);
+  printf("Calling pointer atoi on %s gave: %d\n", input, ptr_atoi(input));
 
 }
 
@@ -24,4 +27,18 @@ int ptr_getline(char *s, int lim) {
     *s++ = c;
   *s = '\0';
   return s - start;
+}
+
+/* ptr_atoi: convert s to integer; ptr version, based on version on P61. */
+int ptr_atoi(char *s) {
+  int n, sign;
+  /* Skip whitespace */
+  while (isspace(*s))
+    s++;
+  sign = (*s == '-') ? -1 : 1;
+  if (*s == '-' || *s == '+') /* Skip sign. */
+    s++;
+  while (isdigit(*s))
+    n = 10 * n + (*s++ - '0');
+  return sign * n;
 }
