23fccb59c99ca6d657b2645b0581ccb72026757c
Crydee <93712280+Crydee@users.noreply.github.com>
2023-12-25 23:30:40 +0000

    Crd/ex5 8 error checking (#56)
    * Add error checking to day_month
    
    * add error checking for day of year calculation
    
    * sort bounds and tidy

---
+++ Ch5/ex5-8/error_checking.c
@@ -0,0 +1,70 @@
+/* There is no error checking of day_of_year or month_day.  Remedy this defect. */
+
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+int main() {
+  int month, day, dayofyear = 366, year = 1884;
+
+  printf("What day of what month was day %d of %d?\n", dayofyear, year);
+  month_day(year, dayofyear, &month, &day);
+  printf("Day: %d, Month: %d\n", day, month);
+
+  printf("What day of the year was on day %d of month %d of year %d?\n", day, month, year);
+  dayofyear = day_of_year(year, month, day);
+  printf("Day %d\n", dayofyear);
+
+}
+
+static char daytab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day. */
+int day_of_year(int year, int month, int day) {
+  int i, leap;
+
+  if (year <= 0) {
+    printf("Please provide a positive numbered year.\n");
+    return -1;
+  } else if (month < 1 || month > 12) {
+    printf("Please provide a month number between 1 and 12.\n");
+    return -1;
+  }
+
+  leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+
+  if (day < 1 || day > daytab[leap][month])
+    printf("Please provide a day that is actually in the specified month.\n");
+  for (i = 1; i < month; i++)
+    day += daytab[leap][i];
+  return day;
+}
+
+/* month_day: set month, day from day of year. */
+void month_day(int year, int yearday, int *pmonth, int *pday) {
+  int i, leap;
+
+  if (yearday < 1) {
+    printf("Please provide a positive number of days\n");
+    return;
+  } else if (year < 0) {
+    printf("Please provide a non-negative year.\n");
+    return;
+  }
+
+  leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
+  for (i = 1; yearday > daytab[leap][i] && i < 13; i++)
+    yearday -= daytab[leap][i];
+  if (i >= 13) {
+    printf("Please provide a number of days that fits within the specified year\n");
+    *pmonth = -1;
+    *pday = -1;
+  } else {
+  *pmonth = i;
+  *pday = yearday;
+  }
+}
