f407ce38ad625d60eb540927291aed7a9cb0c176
Crydee <oscarsterland@yahoo.co.uk>
2024-04-26 21:24:32 +0100

    Small progress

--- Ch5/ex5-14/reverse_sort.c
+++ Ch5/ex5-14/reverse_sort.c
@@ -3,10 +3,12 @@
 #include <string.h>
 
 #define MAXLINES 5000     /* max # lines to be sorted */
+#define REVERSE 1
+#define NUMERIC 2
 char *lineptr[MAXLINES];  /* pointers to text lines */
 
 int readlines(char *lineptr[], int nlines);
-void writelines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines, int reverse);
 void reverse_lines(char *lineptr[], int nlines);
 
 void qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
@@ -16,31 +18,43 @@ int numcmp(char *, char *);
 int main(int argc, char *argv[]) 
 {
   int nlines;   /* number of input lines. */
-  int numeric = 0;    /* 1 if numeric sort */
-  int reverse = 0;    /* 1 if print in reverse */
+  int options = 0;  /* bit encoding of the command line options. */
   int c;
 
   while (--argc > 0 && (*++argv)[0] == '-')
     while (c = *++argv[0])
       switch (c) {
         case 'n':
-          numeric = 1;
+          options |= NUMERIC;
           break;
         case 'r':
-          reverse = 1;
+          options |= REVERSE;
           break;
         default:
           printf("-n for numeric search, -r to print in reverse order\n");
       }
+  if (argc)
+    printf("Usage: sort -nr\n");
 
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    qsort((void **) lineptr, 0, nlines - 1, (int (*)(void*, void*))(numeric ? numcmp: strcmp));
-    if (reverse)
-      reverse_lines(lineptr, nlines);
-    writelines(lineptr, nlines);
+    qsort((void **) lineptr, 0, nlines - 1, (int (*)(void*, void*))((options & NUMERIC) ? numcmp: strcmp));
+    writelines(lineptr, nlines, options & REVERSE);
     return 0;
   } else {
     printf("input too bit to sort\n");
     return 1;
   }
+}
+
+/* writelines: write out nlines lies pointed to by the ptrs in lineptr. */
+void writelines(char *lineptr[], int nlines, int reverse) {
+  if (reverse) 
+    while (nlines)
+      printf("%s", lineptr[nlines--]);
+  else {
+    int count = 0;
+    while (count < nlines)
+      printf("%s", lineptr[count++]);
+  }
+
 }
